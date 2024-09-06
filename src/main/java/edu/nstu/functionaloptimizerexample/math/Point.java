3f42e56e7565647d092e7b98c5f768d4e754430a
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-06 17:48:45 +0700

    style: using lombok annotations instead of getter and setters

--- src/main/java/edu/nstu/functionaloptimizerexample/math/Point.java
+++ src/main/java/edu/nstu/functionaloptimizerexample/math/Point.java
@@ -1,28 +1,11 @@
 package edu.nstu.functionaloptimizerexample.math;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Point {
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Point() {
-    }
     private double x, y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
 }
