820e66bfe3300db3eb198223605afe9e8f2c884b
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-27 15:58:19 +0700

    feat: added required constructors to custom Vector class

--- src/main/java/edu/nstu/functionaloptimizerexample/math/Vector.java
+++ src/main/java/edu/nstu/functionaloptimizerexample/math/Vector.java
@@ -1,7 +1,18 @@
 package edu.nstu.functionaloptimizerexample.math;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Vector extends ArrayList<Double> implements IVector {
+    public Vector(List<Double> list) {
+        super(list);
+    }
 
+    public Vector (int size) {
+        super(size);
+    }
+
+    public Vector() {
+        super();
+    }
 }
