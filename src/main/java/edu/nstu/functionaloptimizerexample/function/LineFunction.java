05ce7612f194d9d2054e9383096edb1e351ab070
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-27 16:02:08 +0700

    feat: now linear function is in n-dimensional space

--- src/main/java/edu/nstu/functionaloptimizerexample/function/LineFunction.java
+++ src/main/java/edu/nstu/functionaloptimizerexample/function/LineFunction.java
@@ -1,25 +1,38 @@
 package edu.nstu.functionaloptimizerexample.function;
 
 import edu.nstu.functionaloptimizerexample.math.IVector;
+import edu.nstu.functionaloptimizerexample.math.Vector;
 
 
 public class LineFunction implements IParametricFunction {
 
-    private static class InternalLineFunction implements IFunction {
-        private double a;
-        private double b;
+    private static class InternalLineFunction implements IDifferentiableFunction {
+        private final IVector parameters;
+
+        public InternalLineFunction(IVector parameters) {
+            this.parameters = parameters;
+        }
 
         @Override
-        public double value(IVector point) {
-            return a * point.get(0) + b;
+        public double value(IVector points) {
+            double sum = parameters.get(0);
+            for(int i = 0; i < points.size(); i++) {
+                sum += parameters.get(i + 1) * points.get(i);
+            }
+            return sum;
+        }
+
+        @Override
+        public IVector gradient(IVector point) {
+            IVector result = new Vector(point);
+            result.addAll(parameters);
+            result.remove(0);
+            return result;
         }
     }
 
     @Override
-    public IFunction bind(IVector parameters) {
-        InternalLineFunction internalFunction = new InternalLineFunction();
-        internalFunction.a = parameters.get(0);
-        internalFunction.b = parameters.get(1);
-        return internalFunction;
+    public IDifferentiableFunction bind(IVector parameters) {
+        return new InternalLineFunction(parameters);
     }
 }
