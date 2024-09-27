ebc420f2352c473d2e54acd0f54fbe82158622b1
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-09-27 15:54:00 +0700

    refactor: MinimizerMonteCarlo minimize is refactored

--- src/main/java/edu/nstu/functionaloptimizerexample/optimizator/MinimizerMonteCarlo.java
+++ src/main/java/edu/nstu/functionaloptimizerexample/optimizator/MinimizerMonteCarlo.java
@@ -5,34 +5,60 @@ import edu.nstu.functionaloptimizerexample.function.IParametricFunction;
 import edu.nstu.functionaloptimizerexample.functional.IFunctional;
 import edu.nstu.functionaloptimizerexample.math.IVector;
 import edu.nstu.functionaloptimizerexample.math.Vector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 
 public class MinimizerMonteCarlo implements IOptimizator {
+    private final static Logger logger = LoggerFactory.getLogger(MinimizerMonteCarlo.class);
+
     private static final int MAX_ITER = 100000;
+
     @Override
-    public IVector minimize(
-            IFunctional objective,
-            IParametricFunction function,
-            IVector initialParameters,
-            IVector minimumParameters,
-            IVector maximumParameters) {
+    public IVector minimize(IFunctional objective,
+                            IParametricFunction function,
+                            IVector initialParameters,
+                            IVector minimumParameters,
+                            IVector maximumParameters) {
         IVector param = new Vector();
         IVector minParam = new Vector();
-        param.addAll(initialParameters);
-        minParam.addAll(initialParameters);
+
+        for (Double initialParameter : initialParameters) {
+            param.add(initialParameter);
+            minParam.add(initialParameter);
+        }
+
         IFunction fun = function.bind(param);
-        double currentmin = objective.value(fun);
+        double currentMin = objective.value(fun);
         Random rand = new Random(0);
-        for (int i = 0; i < MAX_ITER; i++) {
-            param.replaceAll(ignored -> rand.nextDouble());
+
+        int i;
+        for (i = 0; i < MAX_ITER; i++) {
+            for (int j = 0; j < param.size(); j++) {
+                double randomValue = rand.nextDouble();
+                if (minimumParameters != null && maximumParameters != null) {
+                    randomValue = minimumParameters.get(j) + randomValue * (maximumParameters.get(j) - minimumParameters.get(j));
+                }
+                param.set(j, randomValue);
+            }
+
             double f = objective.value(function.bind(param));
-            if (f < currentmin) {
-                currentmin = f;
-                for (int j = 0; j < param.size(); j++)
+            if (f < currentMin) {
+                currentMin = f;
+                for (int j = 0; j < param.size(); j++) {
                     minParam.set(j, param.get(j));
+                }
             }
         }
+        logger.info("iter number = {}, minimum parameter = {}", i, minParam);
         return minParam;
+    }
+
+    @Override
+    public IVector minimize(IFunctional objective,
+                            IParametricFunction function,
+                            IVector initialParameters) {
+        return minimize(objective, function, initialParameters, null, null);
     }
 }
